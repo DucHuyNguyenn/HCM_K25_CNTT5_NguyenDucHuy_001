b0135d0ff8b58cb29bf2044b1d33a158988934a2
Đức Huy Nguyễn <huyrvt21102007@gmail.com>
2026-06-24 16:15:34 +0700

    project

--- feature/feature_5.py
+++ feature/feature_5.py
@@ -3,7 +3,7 @@
 def search_player_func(players):
     name = input("Nhập tên")
     for player in players:
-        if name == players.name:
+        if name == player.name:
             print("BẢNG CẦU THỦ".center(100,"="))
             print(f"{'ID':<10} | {'Name':<20} | {'Speed':<10} | {'Technique':<10} | {'Goals':<10} | {'AVG Score':<15} | {'Type':<20}")
             print(f"{player.id:<10} | {player.name:<20} | {player.speed_score:<10} | {player.technique_score:<10} | {player.goal_score:<10} | {player.averange_score:<15} | {player.performance_type:<20}")
