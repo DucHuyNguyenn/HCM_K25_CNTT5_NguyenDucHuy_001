0f578dd10397d2cd29dfd1c7a736441f0f7192b8
Đức Huy Nguyễn <huyrvt21102007@gmail.com>
2026-06-24 16:14:50 +0700

    project

--- feature/feature_5.py
+++ feature/feature_5.py
@@ -1,10 +1,12 @@
-from feature.feature_1 import show_all_func
+
 
 def search_player_func(players):
     name = input("Nhập tên")
     for player in players:
         if name == players.name:
-            player.show_all_func
-            break
+            print("BẢNG CẦU THỦ".center(100,"="))
+            print(f"{'ID':<10} | {'Name':<20} | {'Speed':<10} | {'Technique':<10} | {'Goals':<10} | {'AVG Score':<15} | {'Type':<20}")
+            print(f"{player.id:<10} | {player.name:<20} | {player.speed_score:<10} | {player.technique_score:<10} | {player.goal_score:<10} | {player.averange_score:<15} | {player.performance_type:<20}")
+            print("="*100)        
     print("Không có cầu thủ cần tìm")
     return
